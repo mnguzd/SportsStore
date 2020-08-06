60f28371e327c44af6286183e767e8c6896a29fe
mnguzd <roman.kioroglo@gmail.com>
2020-08-06 16:16:21 +0300

    Entity Framework added and some configurations

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -15,18 +11,21 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-          
+
             app.UseDeveloperExceptionPage(); //Вывод информации об исключениях
             app.UseStatusCodePages(); //Вывод обработки ошибок HTTP
             app.UseStaticFiles();  //Для папки wwwroot
-            app.UseMvc(routes=> { });  //Включение инфраструктуры APS.NET Core MVC
-          
+            app.UseMvc(routes => {
+                 routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
+            });  //Включение инфраструктуры APS.NET Core MVC
+
         }
     }
 }
