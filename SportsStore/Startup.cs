e903477ed9d7e3c22d97b789d115b4e53a00e266
mnguzd <roman.kioroglo@gmail.com>
2020-08-06 14:53:59 +0300

    Adding MVC to the empty project

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,20 +15,18 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+          
+            app.UseDeveloperExceptionPage(); //Вывод информации об исключениях
+            app.UseStatusCodePages(); //Вывод обработки ошибок HTTP
+            app.UseStaticFiles();  //Для папки wwwroot
+            app.UseMvc(routes=> { });  //Включение инфраструктуры APS.NET Core MVC
+          
         }
     }
 }
